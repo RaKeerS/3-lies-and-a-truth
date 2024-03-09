ff3351335e4d8ed7d96d3f2e3d24be4e58b1a499
Srikar Naidu <n.srikar22@gmail.com>
2024-03-09 18:47:15 +0530

    feat(app): changing project folder hierarchies

--- src/app/views/playground/playground.component.ts
+++ src/app/views/playground/playground.component.ts
@@ -1,7 +1,7 @@
 import { Component, HostBinding } from '@angular/core';
 
-import { PrimeNgModule } from '../prime-ng/prime-ng.module';
-import { APP_CONTAINER_FLEX_CLASS } from '../types/app-types';
+import { PrimeNgModule } from '../../prime-ng/prime-ng.module';
+import { APP_CONTAINER_FLEX_CLASS } from '../../types/app-types';
 
 @Component({
   selector: 'app-playground',
