bd5c5305bb6bbabeb2ff7ba94344db5b62d9e5c3
Srikar Naidu <n.srikar22@gmail.com>
2024-03-21 00:42:22 +0530

    feat(playground): writing business logic for the game v6

--- src/app/models/playground.model.ts
+++ src/app/models/playground.model.ts
@@ -1,8 +1,9 @@
-import { concat, delay, map, of, tap } from 'rxjs';
+import { concat, delay, fromEvent, interval, of, take, takeUntil, tap } from 'rxjs';
 
 export class PlaygroundModel {
 
   private _gameToss: boolean = true;
+  private _switch: boolean = false;
   private _playerOrder: Map<string, number> = new Map<string, number>();
 
   private _playerOneBetAmount: number = 0;
@@ -20,6 +21,10 @@ export class PlaygroundModel {
     this._gameToss = value;
   }
 
+  get playerOrder(): Map<string, number> {
+    return this._playerOrder;
+  }
+
   get playerOneBetAmount(): number { // Use for data-binding with HTML
     return this._playerOneBetAmount;
   }
@@ -34,6 +39,10 @@ export class PlaygroundModel {
     this._playerTwoBetAmount = value;
   }
 
+  get switch(): boolean {
+    return this._switch;
+  }
+
   public commenceRound() {
     return concat(
       this.toss(),
@@ -44,14 +53,56 @@ export class PlaygroundModel {
     // Toss between Player and Opponent automatically, just show their names as 'Player 1', 'Player 2' as buttons and highlight borders alternatively for like 5 secs and using randomizer just pick between the two Players.
     // Randomizer logic to get Players's order.
 
-    this._playerOrder.set('Player1', 1);
-    this._playerOrder.set('Player2', 2);
+    // this._playerOrder.set('Player1', 1);
+    // this._playerOrder.set('Player2', 2);
+
+
+    const source = interval(1000);
+    const clicks = fromEvent(document, 'click');
+    const result = source.pipe(takeUntil(clicks));
+    // result.subscribe(x => console.log(x));
+
+    const timer = interval(500);
+
+    const getRandomOrder = () => (Math.floor(Math.random() * 2) + 1);
+    const setPlayerOrder = (tossResult: number) => {
+      if(tossResult === 1) {
+        this._switch = true; // NOTE - Player 1 Wins the Toss, starts first!
+        this._playerOrder.set('Player1', 1);
+        this._playerOrder.set('Player2', 2);
+      } else {
+        this._switch = false; // NOTE - Player 2 Wins the Toss, starts first!
+        this._playerOrder.set('Player1', 2);
+        this._playerOrder.set('Player2', 1);
+      }
+    }
 
     this.gameToss = true;
-    return of(this.gameToss).pipe(
+    // return of(this.gameToss).pipe(
+    // // delay(15000),
+    // tap(() => this.gameToss = false),
+    // map(() => this._playerOrder));
+
+    const timer$ = timer.pipe(
+take(10),
+tap(() => this._switch = !this._switch));
+
+  const gameOrder$ = of(this.gameToss).pipe(
+    tap(() => setPlayerOrder(getRandomOrder())),
     delay(5000),
-    tap(() => this.gameToss = false),
-    map(() => this._playerOrder));
+    tap(() => this.gameToss = false));
+    // tap(() => console.log('Hi!')),
+    // delay(5000),
+    // tap(() => console.log('Bye!')),
+    // map(() => (this.gameToss = false, getRandomOrder())),
+    // map(yo => yo));
+
+    return concat(
+      timer$,
+      gameOrder$
+    )
+
+
 
     // const abc = interval(1000);
 
