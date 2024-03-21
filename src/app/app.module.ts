924ecca56b552ffa81f5bc36e801afe167c0746f
Srikar Naidu <n.srikar22@gmail.com>
2024-03-22 00:20:51 +0530

    feat(playground): writing business logic for the game v7
    adding support for browserAnimations via. app.config.ts

---
+++ src/app/app.module.ts
@@ -0,0 +1,45 @@
+// import { CommonModule } from '@angular/common';
+// import { NgModule } from '@angular/core';
+// import { FormsModule } from '@angular/forms';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { RouterModule, RouterOutlet } from '@angular/router';
+
+// import { AppComponent } from './app.component';
+// import { routes } from './app.routes';
+// import { PrimeNgModule } from './prime-ng/prime-ng.module';
+// import { DeckComponent } from './views/deck/deck.component';
+// import { MidZoneComponent } from './views/mid-zone/mid-zone.component';
+// import { OpponentComponent } from './views/opponent/opponent.component';
+// import { PlayerComponent } from './views/player/player.component';
+// import { PlaygroundComponent } from './views/playground/playground.component';
+// import { VoidZoneComponent } from './views/void-zone/void-zone.component';
+
+
+
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     PlaygroundComponent,
+//     OpponentComponent,
+//     MidZoneComponent,
+//     PlayerComponent,
+//     DeckComponent,
+//     VoidZoneComponent,
+//   ],
+//   imports: [
+//     CommonModule,
+//     RouterOutlet,
+//     // RouterOutlet,
+//     RouterModule.forRoot(routes, {
+//       useHash: true
+//     }),
+//     FormsModule,
+//     PrimeNgModule,
+//     PrimeNgModule,
+//     BrowserAnimationsModule,
+//     BrowserModule
+//   ],
+//   bootstrap: [AppComponent]
+// })
+export class AppModule { }
