9ccaa9e697ab9c20896e10d2f91bc39d491c0da4
Srikar Naidu <n.srikar22@gmail.com>
2024-04-15 01:50:32 +0530

    feat(playground): working on the game initiation component v13

--- src/app/models/web-rtc/web-rtc.model.ts
+++ src/app/models/web-rtc/web-rtc.model.ts
@@ -379,10 +379,7 @@ export class WebRtcModel {
       switch(parsedData.gameStage) {
         case PlaygroundGameStage.TOSS: {
           const tossResult = Boolean(+parsedData.message);
-          this._playgroundService.tossCompleted.next(true);
-          this._playgroundService.tossCompleted.complete();
           this._playgroundService.switch.next(tossResult);
-          // this._playgroundService.switch.complete();
           break;
         }
 
